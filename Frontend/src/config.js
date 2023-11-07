46fc2ceddb4309103b2033c877e95260999f5ddf
hsilv <hm.sebastiansilva@gmail.com>
2023-11-07 03:01:52 -0600

    fix: (config) api route

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,5 +1,5 @@
 const appName = 'CookApp';
 /* const serverHost = 'http://localhost:3000/'; */
-const serverHost = 'http://165.227.240.90:3000/';
+const serverHost = 'https://cookapp/api/';
 
 export { appName, serverHost };
