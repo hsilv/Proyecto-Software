b7916736849d4adb4ae86c9f020b4e24370043d6
hsilv <hm.sebastiansilva@gmail.com>
2023-11-07 04:39:46 -0600

    fix: (SignUp) validación de contraseñas

--- Frontend/src/components/Register/Register.jsx
+++ Frontend/src/components/Register/Register.jsx
@@ -1,12 +1,11 @@
-import styles from './Register.module.css';
-import Input from '../../components/Input/Input';
-import Joi from 'joi';
-import useForm from '../../hooks/useForm';
-import Button from '../../components/Button/Button';
-import { NavLink } from 'react-router-dom';
+import styles from "./Register.module.css";
+import Input from "../../components/Input/Input";
+import Joi from "joi";
+import useForm from "../../hooks/useForm";
+import Button from "../../components/Button/Button";
+import { NavLink } from "react-router-dom";
 import { useSignUp } from "../../hooks/useSignUp";
 import registerImage from "/assets/register.jpg";
-
 
 const schema = Joi.object({
   realName: Joi.string().min(3).max(30).required(),
@@ -14,26 +13,25 @@ const schema = Joi.object({
   email: Joi.string()
     .email({ tlds: { allow: false } })
     .required(),
-  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-  confirmPassword: Joi.ref('password'),
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
+  confirmPassword: Joi.ref("password"),
 });
 
 function Register() {
-
-  const {signUp, loading, signUpError} = useSignUp();
+  const { signUp, loading, signUpError } = useSignUp();
 
   const form = useForm(schema, {
-    realName: '',
-    username: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
+    realName: "",
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
   });
 
-   const postSignUp = async (username, password, email, name) => {
+  const postSignUp = async (username, password, email, name) => {
     signUp(username, password, email, name);
-  }
-  
+  };
+
   const handleSignUp = () => {
     if (form.validate()) {
       if (form.values.password !== form.values.confirmPassword) {
@@ -42,20 +40,22 @@ function Register() {
         console.log("Passwords match.");
         postSignUp(form.values.username, form.values.password, form.values.email, form.values.realName);
       }
+    } else {
+      console.log("Mensaje de verificación de campos (las contraseñas no son iguales)");
     }
   };
 
   return (
     <div className={styles.registerContainer}>
       <div className={styles.mainImageContainer}>
-        <img src={registerImage} alt="Register Image"/>
+        <img src={registerImage} alt="Register Image" />
       </div>
       <div className={styles.formContainer}>
         <h1>Join us!</h1>
         <div className={styles.inputsContainer}>
           <Input
             value={form.values.realName}
-            onChange={form.onChange('realName')}
+            onChange={form.onChange("realName")}
             name="realName"
             label="Full Name"
             type="text"
@@ -63,7 +63,7 @@ function Register() {
           />
           <Input
             value={form.values.username}
-            onChange={form.onChange('username')}
+            onChange={form.onChange("username")}
             name="username"
             label="Username"
             type="text"
@@ -71,7 +71,7 @@ function Register() {
           />
           <Input
             value={form.values.email}
-            onChange={form.onChange('email')}
+            onChange={form.onChange("email")}
             name="email"
             label="Email Address"
             type="email"
@@ -79,7 +79,7 @@ function Register() {
           />
           <Input
             value={form.values.password}
-            onChange={form.onChange('password')}
+            onChange={form.onChange("password")}
             name="password"
             label="Password"
             type="password"
@@ -87,7 +87,7 @@ function Register() {
           />
           <Input
             value={form.values.confirmPassword}
-            onChange={form.onChange('confirmPassword')}
+            onChange={form.onChange("confirmPassword")}
             name="confirmPassword"
             label="Confirm Password"
             type="password"
@@ -97,15 +97,34 @@ function Register() {
         <Button
           type="primary"
           onClick={handleSignUp}
-          disabled={!form.values.username || !form.values.password || !form.values.email || !form.values.confirmPassword}
+          disabled={
+            !form.values.username ||
+            !form.values.password ||
+            !form.values.email ||
+            !form.values.confirmPassword
+          }
           loading={loading}
         >
           Sign Up
         </Button>
-        {signUpError && <span>{signUpError.message === 'Sesión expirada' ? '' : signUpError.message}</span>}
-        <p style={{ fontFamily: 'League Spartan, sans-serif', fontSize: '14px' }}>
-          Already have an account?{' '}
-          <NavLink to={'/'} style={{ fontFamily: 'League Spartan, sans-serif', fontSize: '14px' }}>
+        {signUpError && (
+          <span>
+            {signUpError.message === "Sesión expirada"
+              ? ""
+              : signUpError.message}
+          </span>
+        )}
+        <p
+          style={{ fontFamily: "League Spartan, sans-serif", fontSize: "14px" }}
+        >
+          Already have an account?{" "}
+          <NavLink
+            to={"/"}
+            style={{
+              fontFamily: "League Spartan, sans-serif",
+              fontSize: "14px",
+            }}
+          >
             Sign in
           </NavLink>
         </p>
